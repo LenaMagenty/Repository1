972401e721216ec7b444dbe02a6f2c821a9727f3
Elena <lena_magenty@gmail.com>
2025-12-24 18:29:01 +0300

    review fixes

--- tests/test_steam.py
+++ tests/test_steam.py
@@ -1,5 +1,4 @@
-import random
-import string
+from faker import Faker
 
 import pytest
 from selenium import webdriver
@@ -8,56 +7,80 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+class DocumentReadyStateComplete:
+    def __call__(self, driver):
+        state = driver.execute_script("return document.readyState")
+        return state == 'complete'
+
+
+WAIT_TIMEOUT = 15
+URL = 'https://store.steampowered.com/'
+LOGIN_BUTTON_LOCATOR = (By.XPATH, "//*[@id='global_action_menu']//a[contains(@href,'login')]")
+LOGIN_PAGE_LOCATOR = (By.XPATH, "//*[contains(@type,'password')]")
+USERNAME_INPUT_LOCATOR = (By.XPATH, "(//form)[2]//input[contains(@type,'text')]")
+PASSWORD_INPUT_LOCATOR = (By.XPATH, "//*[contains(@type,'password')]")
+SUBMIT_BUTTON_LOCATOR = (By.XPATH, "(//button[@type='submit'])[2]")
+SUBMIT_BUTTON_DIS_LOCATOR = (By.XPATH, "//button[@type='submit' and @disabled]")
+SUBMIT_BUTTON_NOT_DIS_LOCATOR = (By.XPATH, "//button[@type='submit' and not(@disabled)]")
+ERROR_TEXT_LOCATOR = (By.XPATH, "//div[contains(@class, '_1W_6HXiG4JJ0By1qN_0fGZ')]")
+EXPECTED_ERROR_TEXT = 'Please check your password and account name and try again.'
+
+
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--window-size=1920,1080')
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
 
-def random_string(length=10):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-
-
 def test_steam(driver):
-    wait = WebDriverWait(driver, 15)
+    wait = WebDriverWait(driver, WAIT_TIMEOUT)
 
     # 1. Открываем главную страницу
     driver.get('https://store.steampowered.com/')
-    wait.until(EC.title_contains('Steam'))
+    WebDriverWait(driver, WAIT_TIMEOUT).until(DocumentReadyStateComplete())
 
     # 2. Нажимаем кнопку Войти
     login_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//*[@id='global_action_menu']//a[contains(@href,'login')]")))
+        EC.element_to_be_clickable(LOGIN_BUTTON_LOCATOR))
     login_button.click()
 
     # 3. Ждём загрузку страницы с логином и паролем
     wait.until(
-        EC.presence_of_element_located((By.XPATH, "//*[contains(@type,'password')]"))
+        EC.presence_of_element_located(LOGIN_PAGE_LOCATOR)
     )
 
     # 4. Вводим рандомный логин и пароль и нажимаем кнопку Войти
-    username = random_string()
-    password = random_string()
-    driver.find_element(By.XPATH, "(//form)[2]//input[contains(@type,'text')]").send_keys(username)
-    driver.find_element(By.XPATH, "//*[contains(@type,'password')]").send_keys(password)
+    faker = Faker()
+    username = faker.user_name()
+    password = faker.password()
+    driver.find_element(*USERNAME_INPUT_LOCATOR).send_keys(username)
+    driver.find_element(*PASSWORD_INPUT_LOCATOR).send_keys(password)
     submit_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "(//button[@type='submit'])[2]")))
+        EC.element_to_be_clickable(SUBMIT_BUTTON_LOCATOR))
     submit_button.click()
 
     # 5. Идёт загрузка, кнопка Войти становится неактивной
     wait.until(
-        EC.presence_of_element_located((By.XPATH, "//button[@type='submit' and @disabled]"))
+        EC.presence_of_element_located(SUBMIT_BUTTON_DIS_LOCATOR)
     )
 
     # 6. Кнопка Войти снова активна
     wait.until(
-        EC.presence_of_element_located((By.XPATH, "//button[@type='submit' and not(@disabled)]"))
+        EC.presence_of_element_located(SUBMIT_BUTTON_NOT_DIS_LOCATOR)
     )
 
     # 7. Появляется сообщение об ошибке
-    error_text = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//button[@type='submit']/parent::div/following-sibling::div[1]"))
+    error_text = wait.until(EC.presence_of_element_located(ERROR_TEXT_LOCATOR))
+
+    actual_error_text = wait.until(lambda d: (
+            error_text.get_attribute('textContent') or ''
+    ).strip())
+
+    assert actual_error_text == EXPECTED_ERROR_TEXT
+
+    assert actual_error_text == EXPECTED_ERROR_TEXT, (
+        f'Ожидаемый текст ошибки: {EXPECTED_ERROR_TEXT}, но получен: {actual_error_text}'
     )
-    assert error_text.is_displayed()
